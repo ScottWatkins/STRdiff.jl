9371ddb6bfcc71a79b47a20463856027caf9c20a
Scott Watkins <scott.watkins@utah.edu>
2020-09-23 12:40:31 -0600

    Add files via upload

--- src/repSTRdiff.jl
+++ src/repSTRdiff.jl
@@ -2,7 +2,7 @@
     repSTRdiff("infile.txt", [hapsize_1, hapsize_2, ... hapsize_N];
         keepall=false, showhaps=false, min_uss_score=10)
 
-Run getSTRdiff for multiple user specified haplotype sizes.
+Run getSTRdiff for multiple user-specified haplotype sizes.
 Retrieve the best estimates from all runs and output a final table.
 See getSTRdiff for additional details.
 
@@ -14,10 +14,18 @@ Options:
 
     min_uss_score  Minimum value for the unique solution score.  For values
                    below this number, both chromosomes from each of the
-                   grandparents will be tested and the minimum uss score for
-                   the comparisons will be shown parenthetically, thus allowing
-                   a potential transmitting parent to be found even though
-                   no single chromosome solution produced a high score [10].
+                   grandparents will be tested, and the minimum difference among
+                   probabilities for both male and both female grandparental
+                   chromosomes will be shown parenthetically next to the uss
+                   score (e.g. 3,(17)).  This allows the sex of the transmitter
+                   to be determined in rare cases where there is high similarity 
+                   between the transmitting person's chromosomes and no single
+                   chromosome solution produced a high uss score.  For example,
+                   [0.91, 0.90, 0.40, 0.73] would return (17) indicating that
+                   the first grandparent (male) is likely to transmit the mutation
+                   even though the single chromosome solution is low.
+                   This value is shown only when the uss score for the best estimate
+                   falls below the min_uss_score setting [default = 10].
 
 """
 function repSTRdiff(infile::String, hapsizes::Array; keepall=false, showhaps=false, min_uss_score=10)
@@ -83,11 +91,9 @@ function repSTRdiff(infile::String, hapsizes::Array; keepall=false, showhaps=fal
                     loc = l[3]
 
                     if (t + o) > (uss + off)
-
                         best = i
                         uss = t
                         off = o
-
                     end
 
                 else
@@ -102,40 +108,34 @@ function repSTRdiff(infile::String, hapsizes::Array; keepall=false, showhaps=fal
                 best = "$ped\t$iid\t$loc\toptimal solution not found"
             end
 
-            function uss_check(best)
 
-                if (length(best) > 70) && (!occursin(r"\t-9\t", best))
-
+            function uss_check(best, buss, min_uss_score)
+                if (buss ≤ min_uss_score) && (length(best) > 70) && (!occursin(r"\t-9\t", best))
                     b = split(best, '\t')
                     s = parse(Int64, b[14])
-
-                    if s ≤ min_uss_score
-                        
-                        c = replace(b[13], "[" => "")
-                        c = replace(c, "]" => "")
-                        p = parse.(Float64, split(c, r", "))
-                        d1 = abs(p[1] - p[3])
-                        d2 = abs(p[1] - p[4])
-                        d3 = abs(p[2] - p[3])
-                        d4 = abs(p[2] - p[4])
-                        
-                        if (d1 ≥ 0.1 && d2 ≥ 0.10 && d3 ≥ 0.1 && d4 ≥ 0.1)
-                            uss2 = floor(Int64,(minimum([d1, d2, d3, d4]) * 100))
-                        end
-                        
-                        b[14] = join([ s, ",", "(", uss2, ")" ],"")
-                        best = ( join(b, "\t") )
-
-                    end
-
+                    c = replace(b[13], "[" => "")
+                    c = replace(c, "]" => "")
+                    p = parse.(Float64, split(c, r", "))
+                    d1 = abs(p[1] - p[3])
+                    d2 = abs(p[1] - p[4])
+                    d3 = abs(p[2] - p[3])
+                    d4 = abs(p[2] - p[4])
+#                    println("$d1  $d2  $d3  $d4")
+                    uss2 = floor(Int64,(minimum([d1, d2, d3, d4]) * 100))
+                    b[14] = join([ s, ",", "(", uss2, ")" ],"")
+                    best2 = ( join(b, "\t") )
+                else
+                    best2 = best
                 end
-
-                return best
-
+                return best2
             end
 
-            best = uss_check(best)
-            
+            if length(best) > 70             #Retest grandparental chromosomes together
+                g = split(best, '\t')
+                buss = parse(Int64, g[14])
+                best = uss_check(best, buss, min_uss_score)
+            end
+
             println(OUT, best)
 
         end
